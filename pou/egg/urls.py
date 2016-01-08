d0ae23cb30e2444ad78910d424f20132d2dad311
Ilya Lyaukin <kzerby@gmail.com>
2016-01-08 17:03:59 +0300

    Add environments; Remove X-Frame-Options

--- pou/egg/urls.py
+++ pou/egg/urls.py
@@ -7,4 +7,4 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^(?P<egg_id>\d+)/$', views.egg, name='egg'),
                        url(r'^hit/(?P<egg_id>\d+)/$', views.hit, name='hit'),
-)
+                       )
--- pou/pou/settings.py
+++ pou/pou/settings.py
@@ -10,78 +10,95 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from configurations import Configuration
+
+
+class Base(Configuration):
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+class Dev(Base):
+    BASE_DIR = Base.BASE_DIR
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+3)zodgcj)8sp!r=wm)h()@_hpg5%*eb!kvamh76%9e=3_e1l('
+    SECRET_KEY = '+3)zodgcj)8sp!r=wm)h()@_hpg5%*eb!kvamh76%9e=3_e1l('
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    DEBUG = True
 
-TEMPLATE_DEBUG = True
+    TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = []
 
+    # Application definition
 
-# Application definition
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'egg',
+    )
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'egg',
-)
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+    ROOT_URLCONF = 'pou.urls'
 
-ROOT_URLCONF = 'pou.urls'
+    WSGI_APPLICATION = 'pou.wsgi.application'
 
-WSGI_APPLICATION = 'pou.wsgi.application'
+    # Database
+    # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+    TIME_ZONE = 'UTC'
 
-USE_I18N = True
+    USE_I18N = True
 
-USE_L10N = True
+    USE_L10N = True
 
-USE_TZ = True
+    USE_TZ = True
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+    STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+class Production(Dev):
+    DEBUG = False
 
-STATIC_URL = '/static/'
+    ALLOWED_HOSTS = ['.pythonanywhere.com']
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': 'kzerby.mysql.pythonanywhere-services.com',
+            'NAME': 'kzerby$egg',
+            'USER': 'kzerby',
+            'PASSWORD': os.environ.get('EGG_SECRET', ''),
+        }
+    }
